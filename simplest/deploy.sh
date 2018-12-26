0fd6cc42c8fced2f64422fc981cf906ede52bb49
Arsen Mamikonyan <arsen@mamikonyan.am>
2018-12-26 14:00:21 +0400

    Enable services

--- simplest/deploy.sh
+++ simplest/deploy.sh
@@ -4,10 +4,11 @@ set -e
 set -x
 
 project_name=helloworld_0_simplest
+
+
+# Copy code to the destination directory where it will run
+# The service will run with user www-data
 destination=/var/$project_name
-
-sudo cp $project_name.service /etc/systemd/system/
-
 
 sudo mkdir -p $destination
 sudo chown -R www-data:www-data $destination
@@ -19,6 +20,13 @@ sudo -u www-data rsync -ra \
   $destination
 
 
-# Reload and restart services
+# Copy systemd files to the correct location.
+sudo cp $project_name.service /etc/systemd/system/
+
+# Reload systemctl
 sudo systemctl daemon-reload
+# Enable the service so it's there when the comuper restarts.
+sudo systemctl enable $project_name.service
+
+# restart services
 sudo service $project_name reload || sudo service $project_name start
